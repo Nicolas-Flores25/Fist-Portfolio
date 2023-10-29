74218e6549d216b86b31792642044655f0fba194
Nicolas <nicolas.flores.eguizabal.25@gmail.com>
2023-10-29 15:50:26 -0600

    fixes in link menu

--- JS/menu.js
+++ JS/menu.js
@@ -1,14 +1,20 @@
 (function(){
     const openButton = document.querySelector('.nav_menu');
     const menu = document.querySelector('.nav_link');
-    const closeMenu = document.querySelector('.nav_close')
+    const closeMenu = document.querySelector('.nav_close');
+    const menuLinks = menu.querySelectorAll('a'); // Selecciona tolos los links
 
-    openButton.addEventListener('click',() =>{
-        menu.classList.add('nav_link--show');  
+    openButton.addEventListener('click', () => {
+        menu.classList.add('nav_link--show');
     });
 
-    closeMenu.addEventListener('click',()=>{
-        menu.classList.remove('nav_link--show')
-    })
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove('nav_link--show');
+    });
 
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('nav_link--show');
+        });
+    });
 })();
